29ba5ea909bf70174813bc93a9cbfeaafc4631e3
Taras <45391115+prystupataras@users.noreply.github.com>
2024-12-30 21:57:56 +0200

    Initial Commit

---
+++ ImageTextSwiftUI/ImageTextSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageTextSwiftUIApp.swift
+//  ImageTextSwiftUI
+//
+//  Created by Taras Prystupa on 30.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageTextSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
